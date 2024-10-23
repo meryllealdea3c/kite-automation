72d69ee22f576eb79f4a7cf05bb55291a4f7013f
Ghoudz Estepa <gestepa@3cloudsolutions.com>
2024-10-23 21:01:38 +0800

    Initial code

---
+++ Kite-API-Automation/MainClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+
+namespace Kite_API_Automation
+{
+    [TestFixture]
+    public class MainClass
+    {
+        [Test]
+        public void Test()
+        {
+            Functions.GetResponseCode().Wait();
+        }
+    }
+}
